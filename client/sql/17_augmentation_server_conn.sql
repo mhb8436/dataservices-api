0b6b87925676a16a794b498140fb6c7997e764d5
Carla <carla@cartodb.com>
2016-06-10 19:07:09 +0200

    Update 17_augmentation_server_conn.sql

--- client/sql/17_augmentation_server_conn.sql
+++ client/sql/17_augmentation_server_conn.sql
@@ -13,3 +13,18 @@ BEGIN
   RETURN db_connection_json;
 END;
 $$ LANGUAGE 'plpgsql';
+
+--
+-- Augmentation server connection string for Pl/Proxy connection intended to drop temporary table.
+--
+
+CREATE OR REPLACE FUNCTION _augmentation_server_conn_str()
+RETURNS text AS $$
+DECLARE
+  db_connection_str text;
+BEGIN
+  SELECT cartodb.cdb_conf_getconf('fdw_server_config')->'connection_str' INTO db_connection_str;
+  SELECT trim(both '"' FROM db_connection_str) INTO db_connection_str;
+  RETURN db_connection_str;
+END;
+$$ LANGUAGE 'plpgsql';
