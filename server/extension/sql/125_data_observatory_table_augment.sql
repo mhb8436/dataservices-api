220505c083914d61ad8304eebb5ad4dcb106f671
Carla <carla@cartodb.com>
2016-06-10 19:11:08 +0200

    Update 125_data_observatory_table_augment.sql

--- server/extension/sql/125_data_observatory_table_augment.sql
+++ server/extension/sql/125_data_observatory_table_augment.sql
@@ -2,19 +2,7 @@
 -- Temporary server functions which should live in OBS.
 --
 
-CREATE OR REPLACE FUNCTION _wipe_user_augmented_table(fdw_schema text, table_name text)
-RETURNS boolean AS $$
-DECLARE
-  drop_query text;
-  users text;
-BEGIN
-  SELECT session_user INTO users;
-  RAISE NOTICE 'USER: % [DS Server] Dropping augmented table %.%', users, fdw_schema, table_name;
-  drop_query := 'DROP TABLE IF EXISTS ' || fdw_schema || '.' || table_name;
-  RETURN true;
-END;
-$$ LANGUAGE plpgsql;
-
+CREATE TYPE table_augment_metadata as (schemaname text, tabname text);
 
 CREATE OR REPLACE FUNCTION _OBS_AugmentWithMeasureFDW(username text, useruuid text, input_schema text, dbname text, host text, table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
 RETURNS table_augment_metadata
@@ -30,10 +18,13 @@ DECLARE
   grant_query text;
   grant2_query text;
   return_query text;
+  epoch_timestamp text;
   obs_result boolean;
 BEGIN
 
-  temp_table_name := 'aug_' || table_name || '_tmp';
+  SELECT extract(epoch from now() at time zone 'utc')::int INTO epoch_timestamp;
+  -- Temporal naming to avoid collisions
+  temp_table_name := 'aug_' || table_name || '_tmp_' || epoch_timestamp;
   fdw_server := 'fdw_server' || username;
   fdw_schema:= 'fdw_' || username;
 
@@ -53,6 +44,7 @@ BEGIN
                 || ') FROM SERVER "' || fdw_server || '" INTO "'
                 || fdw_schema
                 || '";';
+
   EXECUTE query_import;
 
   -- Call to Observatory function that will generate a table with a given name
@@ -62,7 +54,7 @@ BEGIN
 
   IF obs_result THEN
     -- TODO: add index on cartodb_id
-    grant_query = 'GRANT SELECT ON "' || fdw_schema || '".' || temp_table_name || ' TO fdw_user;';
+    grant_query = 'ALTER TABLE "' || fdw_schema || '".' || temp_table_name || ' OWNER TO fdw_user;';
     grant2_query = 'GRANT USAGE ON SCHEMA "' || fdw_schema || '" TO fdw_user;';
     EXECUTE grant_query;
     EXECUTE grant2_query;
@@ -71,26 +63,47 @@ BEGIN
   RAISE NOTICE '[DS Server] OBS result: %', obs_result;
 
   -- Disconnect user table
-  RAISE NOTICE '[DS Server] Dropping foreign table';
   EXECUTE 'DROP FOREIGN TABLE IF EXISTS "' || fdw_schema || '".' || table_name;
-
   -- Return server and table information
-  RETURN (fdw_schema, temp_table_name);
+  IF obs_result THEN
+    RETURN (fdw_schema, temp_table_name);
+  ELSE
+    RETURN (''::text, ''::text);
+  END IF;
 
 EXCEPTION
   WHEN others THEN
     RAISE NOTICE '[DS Server] Something failed (errcode: %, errm: %)', SQLSTATE, SQLERRM;
-    RAISE NOTICE '[DS Server] Dropping foreign table';
     EXECUTE 'DROP FOREIGN TABLE IF EXISTS "' || fdw_schema || '".' || table_name;
-    RETURN (null, null);
+    RETURN (''::text, ''::text);
 
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
+
+
+CREATE OR REPLACE FUNCTION _wipe_user_augmented_table(fdw_schema text, table_name text)
+RETURNS boolean AS $$
+DECLARE
+  drop_query text;
+  users text;
+BEGIN
+  SELECT session_user INTO users;
+  drop_query := 'DROP TABLE IF EXISTS ' || fdw_schema || '.' || table_name;
+  RAISE NOTICE 'USER: % [DS Server] %', users, drop_query;
+  EXECUTE drop_query;
+  RETURN true;
+EXCEPTION
+  WHEN others THEN
+    RETURN false;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
 --
 -- Mock for observatory function.
 --
-
 CREATE OR REPLACE FUNCTION observatory._OBS_AugmentWithMeasureFDW(aug_schema text, input_table_name text, aug_table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
 RETURNS boolean
 AS $$
@@ -99,6 +112,8 @@ DECLARE
 BEGIN
 
   -- Create temp table with data results
+  -- If the table is temporary, it can be in a specified schema
+  -- ERROR:  cannot create temporary relation in non-temporary schema
   data_query := 'CREATE TABLE '
         || '"' || aug_schema || '".' || aug_table_name
         || ' AS (WITH _areas AS(SELECT ST_Area(a.the_geom::geography)'
@@ -114,16 +129,16 @@ BEGIN
         || tag_name
         || ', cartodb_id FROM _areas, values '
         || 'WHERE values.geoid = _areas.geoid GROUP BY cartodb_id);';
-
-  RAISE NOTICE '[OBS Server] Creating table AS: %', data_query;
   EXECUTE data_query;
 
   RETURN true;
+
 EXCEPTION
   WHEN others THEN
     RAISE NOTICE '[OBS Server] Something failed (errcode: %, errm: %)', SQLSTATE, SQLERRM;
     RAISE NOTICE '[OBS Server] Dropping temp table';
     EXECUTE 'DROP TABLE IF EXISTS "' || aug_schema || '".' || aug_table_name;
     RETURN false;
+
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
