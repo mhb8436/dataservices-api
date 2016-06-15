08cf70b4e78a76f913d4eac5afdcb94365100780
Carla <carla@cartodb.com>
2016-06-15 18:10:16 +0200

    Prototype zero++

--- client/sql/20_table_augmentation.sql
+++ client/sql/20_table_augmentation.sql
@@ -1,12 +1,7 @@
-CREATE TYPE table_augment_data as (schemaname text, tabname text, servername text, colnames text[], coltypes text[]);
+CREATE TYPE ds_fdw_metadata as (schemaname text, tabname text, servername text);
+CREATE TYPE ds_return_metadata as (colnames text[], coltypes text[]);
 
---
--- Public function to augment a table with a new measure column.
--- This function checks that the user is authenticated and calls to 
--- the internal function which will manage the process.
---
-
-CREATE OR REPLACE FUNCTION OBS_AugmentMeasure2(table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
+CREATE OR REPLACE FUNCTION OBS_AugmentTable(table_name text, output_table_name text, params json)
 RETURNS boolean AS $$
 DECLARE
   username text;
@@ -38,60 +33,76 @@ BEGIN
   SELECT current_database() INTO dbname;
   SELECT _get_db_host() INTO hostname;
 
-  SELECT _OBS_AugmentMeasure2(username::text, useruuid::text, input_schema::text, dbname::text, hostname::text, table_name::text, column_name::text, tag_name::text, normalize::text, timespan::text, geometry_level::text) INTO result;
+  SELECT _OBS_AugmentTable(username::text, useruuid::text, input_schema::text, dbname::text, hostname::text, table_name::text, output_table_name::text, params::json) INTO result;
 
   RETURN true;
 END;
 $$ LANGUAGE 'plpgsql' SECURITY DEFINER;
 
---
--- Internal function to augment a table with a measure column. Handles the 
--- communication with the Data Services server and updates the local table
--- of the user with data obtained through FDW.
---
 
 
-
-CREATE OR REPLACE FUNCTION _OBS_AugmentMeasure2(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
+CREATE OR REPLACE FUNCTION _OBS_AugmentTable(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text, output_table_name text, params json)
 RETURNS boolean AS $$
     try:
-        # Call to augment server, colnames and coltypes are arrays
-        #fdw_metadata, connectUserTable
-        augment_metadata = plpy.execute("SELECT schemaname, tabname, servername, colnames, coltypes FROM _OBS_AugmentMeasureServer2('{0}'::text, '{1}'::text, '{2}'::text, '{3}'::text, '{4}'::text, '{5}'::text, '{6}'::text, '{7}'::text, '{8}'::text, '{9}'::text, '{10}'::text);".format(username, useruuid, input_schema, dbname, hostname, table_name, column_name, tag_name, normalize, timespan, geometry_level))
-        # divide in setup fdw + metadata
-        epoch_timestamp = plpy.execute("SELECT extract(epoch from now() at time zone 'utc')::int as epoch")[0]["epoch"]
-        schemaname = augment_metadata[0]["schemaname"]
-        tabname = augment_metadata[0]["tabname"]
-        servername = augment_metadata[0]["servername"]
-        colnames_array = augment_metadata[0]["colnames"]
-        coltypes_array = augment_metadata[0]["coltypes"]
-        new_table_name = table_name + '_augmented_' + str(epoch_timestamp)
+        # Obtain metadata for FDW connection
+        ds_fdw_metadata = plpy.execute("SELECT schemaname, tabname, servername "
+            "FROM _OBS_ConnectUserTable('{0}'::text, '{1}'::text, '{2}'::text, '{3}'::text, '{4}'::text, '{5}'::text);"
+            .format(username, useruuid, input_schema, dbname, hostname, table_name))
 
-        plpy.execute("CREATE TABLE {0} AS (SELECT results.{1}, user_table.* FROM {3} as user_table, _OBS_AugmentMeasureServer2Results('{2}'::text, '{3}'::text, '{4}'::text, '{5}'::text, '{6}'::text, '{7}'::text) as results({1} numeric, cartodb_id int) WHERE results.cartodb_id = user_table.cartodb_id)".format(new_table_name, colnames_array[0] ,schemaname, tabname, tag_name, normalize, timespan, geometry_level))
-        plpy.execute('ALTER TABLE {0} OWNER TO "{1}";'.format(new_table_name, useruuid))
-        wiped = plpy.execute("SELECT _OBS_DropForeignData('{0}'::text, '{1}'::text, '{2}'::text)".format(schemaname, tabname, servername))
+        schemaname = ds_fdw_metadata[0]["schemaname"]
+        tabname = ds_fdw_metadata[0]["tabname"]
+        servername = ds_fdw_metadata[0]["servername"]
+
+        # Obtain return types for augmentation procedure
+        ds_return_metadata = plpy.execute("SELECT colnames, coltypes "
+            "FROM _OBS_GetReturnMetadata('{0}'::json);"
+            .format(params))
+
+        colnames_array = ds_return_metadata[0]["colnames"]
+        coltypes_array = ds_return_metadata[0]["coltypes"]
+
+        # Populate a new table with the augmented results
+        plpy.execute("CREATE TABLE {0} AS "
+            "(SELECT results.{1}, user_table.* "
+            "FROM {3} as user_table, "
+            "_OBS_GetAugmentedColumns('{2}'::text, '{3}'::text, '{4}'::json) as results({1} numeric, cartodb_id int) "
+            "WHERE results.cartodb_id = user_table.cartodb_id)"
+            .format(output_table_name, colnames_array[0] ,schemaname, tabname, params))
+
+        plpy.execute('ALTER TABLE {0} OWNER TO "{1}";'
+            .format(output_table_name, useruuid))
+
+        # Wipe user FDW data from the server
+        wiped = plpy.execute("SELECT _OBS_DisconnectUserTable('{0}'::text, '{1}'::text, '{2}'::text)"
+            .format(schemaname, tabname, servername))
 
         return True
     except Exception as e:
         plpy.warning('Error trying to augment table {0}'.format(e))
+        # Wipe user FDW data from the server in case of failure
         if tabname:
-            wiped = plpy.execute("SELECT _OBS_DropForeignData('{0}'::text, '{1}'::text, '{2}'::text)".format(schemaname, tabname, servername))
+            wiped = plpy.execute("SELECT _OBS_DisconnectUserTable('{0}'::text, '{1}'::text, '{2}'::text)"
+                .format(schemaname, tabname, servername))
         return False
 $$ LANGUAGE plpythonu;
 
 
-CREATE OR REPLACE FUNCTION _OBS_AugmentMeasureServer2(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text, column_name text, tag_name text, normalize text, timespan text, geometry_level text)
-RETURNS table_augment_data AS $$
+CREATE OR REPLACE FUNCTION _OBS_ConnectUserTable(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text)
+RETURNS ds_fdw_metadata AS $$
     CONNECT _server_conn_str();
-    SELECT schemaname, tabname, servername, colnames, coltypes FROM _OBS_AugmentMeasureServer2(username::text, useruuid::text, input_schema::text, dbname:: text, hostname::text, table_name::text, column_name::text, tag_name::text, normalize::text, timespan::text, geometry_level::text);
 $$ LANGUAGE plproxy;
 
-CREATE OR REPLACE FUNCTION _OBS_AugmentMeasureServer2Results(table_schema text, table_name text, tag_name text, normalize text, timespan text, geometry_level text)
+CREATE OR REPLACE FUNCTION _OBS_GetReturnMetadata(params json)
+RETURNS ds_return_metadata AS $$
+    CONNECT _server_conn_str();
+$$ LANGUAGE plproxy;
+
+CREATE OR REPLACE FUNCTION _OBS_GetAugmentedColumns(table_schema text, table_name text, params json)
 RETURNS SETOF record AS $$
     CONNECT _server_conn_str();
 $$ LANGUAGE plproxy;
 
-CREATE OR REPLACE FUNCTION _OBS_DropForeignData(table_schema text, table_name text, server_name text)
+CREATE OR REPLACE FUNCTION _OBS_DisconnectUserTable(table_schema text, table_name text, server_name text)
 RETURNS boolean AS $$
     CONNECT _server_conn_str();
 $$ LANGUAGE plproxy;
