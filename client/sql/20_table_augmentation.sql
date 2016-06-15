f3f10a12c7b383fde99c042773a1bac311e6032c
Carla <carla@cartodb.com>
2016-06-15 16:39:59 +0200

    Prototype zero

--- client/sql/20_table_augmentation.sql
+++ client/sql/20_table_augmentation.sql
@@ -1,4 +1,4 @@
-CREATE TYPE table_augment_metadata as (schemaname text, tabname text);
+CREATE TYPE table_augment_data as (schemaname text, tabname text, servername text, colnames text[], coltypes text[]);
 
 --
 -- Public function to augment a table with a new measure column.
@@ -6,10 +6,11 @@ CREATE TYPE table_augment_metadata as (schemaname text, tabname text);
 -- the internal function which will manage the process.
 --
 
-CREATE OR REPLACE FUNCTION OBS_AugmentWithMeasure(table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
+CREATE OR REPLACE FUNCTION OBS_AugmentMeasure2(table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
 RETURNS boolean AS $$
 DECLARE
   username text;
+  useruuid text;
   orgname text;
   dbname text;
   hostname text;
@@ -19,6 +20,8 @@ BEGIN
   IF session_user = 'publicuser' OR session_user ~ 'cartodb_publicuser_*' THEN
     RAISE EXCEPTION 'The api_key must be provided';
   END IF;
+
+  SELECT session_user INTO useruuid;
 
   SELECT u, o INTO username, orgname FROM cdb_dataservices_client._cdb_entity_config() AS (u text, o text);
   -- JSON value stored "" is taken as literal
@@ -35,9 +38,9 @@ BEGIN
   SELECT current_database() INTO dbname;
   SELECT _get_db_host() INTO hostname;
 
-  SELECT _OBS_AugmentWithMeasure(username, input_schema, dbname, hostname, table_name, column_name, tag_name, normalize, timespan, geometry_level) INTO result;
+  SELECT _OBS_AugmentMeasure2(username::text, useruuid::text, input_schema::text, dbname::text, hostname::text, table_name::text, column_name::text, tag_name::text, normalize::text, timespan::text, geometry_level::text) INTO result;
 
-  RETURN result;
+  RETURN true;
 END;
 $$ LANGUAGE 'plpgsql' SECURITY DEFINER;
 
@@ -48,159 +51,47 @@ $$ LANGUAGE 'plpgsql' SECURITY DEFINER;
 --
 
 
-CREATE OR REPLACE FUNCTION _OBS_AugmentWithMeasure(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
+
+CREATE OR REPLACE FUNCTION _OBS_AugmentMeasure2(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text, column_name text, tag_name text, normalize text default null, timespan text DEFAULT null, geometry_level text DEFAULT null)
 RETURNS boolean AS $$
     try:
-        local_schema = 'fdw_' + username
-        foreign_table = None
-        foreign_connected = None
-        fdw_server = 'fdw_server_' + username
+        # Call to augment server, colnames and coltypes are arrays
+        #fdw_metadata, connectUserTable
+        augment_metadata = plpy.execute("SELECT schemaname, tabname, servername, colnames, coltypes FROM _OBS_AugmentMeasureServer2('{0}'::text, '{1}'::text, '{2}'::text, '{3}'::text, '{4}'::text, '{5}'::text, '{6}'::text, '{7}'::text, '{8}'::text, '{9}'::text, '{10}'::text);".format(username, useruuid, input_schema, dbname, hostname, table_name, column_name, tag_name, normalize, timespan, geometry_level))
+        # divide in setup fdw + metadata
+        epoch_timestamp = plpy.execute("SELECT extract(epoch from now() at time zone 'utc')::int as epoch")[0]["epoch"]
+        schemaname = augment_metadata[0]["schemaname"]
+        tabname = augment_metadata[0]["tabname"]
+        servername = augment_metadata[0]["servername"]
+        colnames_array = augment_metadata[0]["colnames"]
+        coltypes_array = augment_metadata[0]["coltypes"]
+        new_table_name = table_name + '_augmented_' + str(epoch_timestamp)
 
-        # Call to augment server
-        foreign_metadata = plpy.execute("SELECT schemaname, tabname FROM _OBS_AugmentWithMeasureFDW('{0}'::text, '{1}'::text, '{2}'::text, '{3}'::text, '{4}'::text, '{5}'::text, '{6}'::text, '{7}'::text, '{8}'::text, '{9}'::text, '{10}'::text);".format(username, useruuid, input_schema, dbname, hostname, table_name, column_name, tag_name, normalize, timespan, geometry_level))
+        plpy.execute("CREATE TABLE {0} AS (SELECT results.{1}, user_table.* FROM {3} as user_table, _OBS_AugmentMeasureServer2Results('{2}'::text, '{3}'::text, '{4}'::text, '{5}'::text, '{6}'::text, '{7}'::text) as results({1} numeric, cartodb_id int) WHERE results.cartodb_id = user_table.cartodb_id)".format(new_table_name, colnames_array[0] ,schemaname, tabname, tag_name, normalize, timespan, geometry_level))
+        plpy.execute('ALTER TABLE {0} OWNER TO "{1}";'.format(new_table_name, useruuid))
+        wiped = plpy.execute("SELECT _OBS_DropForeignData('{0}'::text, '{1}'::text, '{2}'::text)".format(schemaname, tabname, servername))
 
-        foreign_schema = foreign_metadata[0]["schemaname"]
-        foreign_table = foreign_metadata[0]["tabname"]
-        if not foreign_table:
-            raise Exception('Server did not provide a table')
-
-        foreign_connected = plpy.execute("SELECT _connect_augmented_table('{0}'::text, '{1}'::text, '{2}'::text, '{3}'::text)".format(username, foreign_schema, foreign_table, local_schema))
-
-        # Get name and type of columns augmented in the server
-        new_columns = plpy.execute('SELECT a.attname as name, format_type(a.atttypid, a.atttypmod) AS data_type FROM pg_attribute a, pg_class b WHERE a.attrelid = b.relfilenode AND a.attrelid = \'\"{0}\".{1}\'::regclass AND a.attnum > 0 AND NOT a.attisdropped AND a.attname NOT LIKE \'cartodb_id\' ORDER BY a.attnum;'.format(foreign_schema, foreign_table))
-
-        plpy.warning('[Client] Connected foreign table {0}.{1}'.format(foreign_schema, foreign_table))
-
-        # Add the augmented column to the user table
-        augmented_column = plpy.execute("SELECT _add_augmented_column('{0}'::text, '{1}'::text, '{2}'::text) as name".format(table_name, new_columns[0]["name"], new_columns[0]["data_type"]))[0]["name"]
-
-        # Update the user table with the augmented data
-        plpy.execute('UPDATE {0} SET {1} = augmented.{2} FROM "{3}".{4} as augmented WHERE {0}.cartodb_id = augmented.cartodb_id;'.format(table_name, augmented_column, new_columns[0]["name"], foreign_schema, foreign_table))
-
-        if foreign_connected:
-            # Clean local table
-            plpy.execute('SELECT _disconnect_foreign_table(\'\"{0}\"\'::text, \'{1}\'::text, \'{2}\'::text)'.format(local_schema, foreign_table, fdw_server))
-        if foreign_table:
-            # Clean remote table
-            plpy.execute('SELECT _mark_augmented_foreign_table_deletion(\'\"{0}\"\'::text, \'{1}\'::text)'.format(foreign_schema, foreign_table))
         return True
     except Exception as e:
         plpy.warning('Error trying to augment table {0}'.format(e))
-        if foreign_connected:
-            # Clean local table
-            plpy.execute('SELECT _disconnect_foreign_table(\'\"{0}\"\'::text, \'{1}\'::text, \'{2}\'::text)'.format(local_schema, foreign_table, fdw_server))
-        if foreign_table:
-            # Clean remote table
-            plpy.execute('SELECT _mark_augmented_foreign_table_deletion(\'\"{0}\"\'::text, \'{1}\'::text)'.format(foreign_schema, foreign_table))
+        if tabname:
+            wiped = plpy.execute("SELECT _OBS_DropForeignData('{0}'::text, '{1}'::text, '{2}'::text)".format(schemaname, tabname, servername))
         return False
 $$ LANGUAGE plpythonu;
 
 
-CREATE OR REPLACE FUNCTION _OBS_AugmentWithMeasureFDW(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text, column_name text, tag_name text, normalize text, timespan text, geometry_level text)
-RETURNS table_augment_metadata AS $$
+CREATE OR REPLACE FUNCTION _OBS_AugmentMeasureServer2(username text, useruuid text, input_schema text, dbname text, hostname text, table_name text, column_name text, tag_name text, normalize text, timespan text, geometry_level text)
+RETURNS table_augment_data AS $$
     CONNECT _server_conn_str();
-    SELECT schemaname, tabname FROM _OBS_AugmentWithMeasureFDW(username::text, useruuid::text, input_schema::text, dbname:: text, hostname::text, table_name::text, column_name::text, tag_name::text, normalize::text, timespan::text, geometry_level::text);
+    SELECT schemaname, tabname, servername, colnames, coltypes FROM _OBS_AugmentMeasureServer2(username::text, useruuid::text, input_schema::text, dbname:: text, hostname::text, table_name::text, column_name::text, tag_name::text, normalize::text, timespan::text, geometry_level::text);
 $$ LANGUAGE plproxy;
 
---
--- Internal function to connect to a foreign table
---
-
-CREATE OR REPLACE FUNCTION _connect_augmented_table(username text, foreign_schema text, foreign_table text, local_schema text)
-RETURNS boolean AS $$
-DECLARE
-  fdw_server text;
-  query_import text;
-  schema_query text;
-  connection_str json;
-BEGIN
-
-  fdw_server := 'fdw_server_' || username;
-
-  SELECT _augmentation_server_conn_json() INTO connection_str;
-
-  -- Configure FDW to Augmentation Server
-  EXECUTE 'SELECT cartodb._CDB_Setup_FDW('''|| fdw_server ||''', $1::json)' USING connection_str ;
-
-  schema_query := 'CREATE SCHEMA IF NOT EXISTS "' || local_schema ||'"';
-  EXECUTE schema_query;
-
-  -- Import result table
-  query_import := 'IMPORT FOREIGN SCHEMA "' || foreign_schema || '" LIMIT TO ('|| foreign_table ||') '
-                || ' FROM SERVER "' || fdw_server || '" INTO "' || local_schema || '";';
-  EXECUTE query_import;
-
-  RETURN true;
-EXCEPTION
-  WHEN others THEN
-    RAISE NOTICE '[Client] Something failed when connecting the foreign table (errcode: %, errm: %)', SQLSTATE, SQLERRM;
-    RETURN false;
-END;
-$$ LANGUAGE plpgsql;
-
---
--- Internal function to disconnect foreign table
---
-
-CREATE OR REPLACE FUNCTION _disconnect_foreign_table(local_schema text, foreign_table text, server_name text)
-RETURNS boolean AS $$
-DECLARE
-  drop_query text;
-  drop_schema text;
-  drop_server text;
-BEGIN
-  -- Drop foreign table
-  drop_query :='DROP FOREIGN TABLE IF EXISTS '|| local_schema ||'.' || foreign_table ;
-  EXECUTE drop_query;
-
-  -- Drop schema
-  drop_schema := 'DROP SCHEMA IF EXISTS ' || local_schema;
-  EXECUTE drop_schema;
-
-  drop_server := 'DROP SERVER ' || server_name || ' CASCADE;';
-  EXECUTE drop_server;
-
-  RETURN true;
-EXCEPTION
-  WHEN others THEN
-    RAISE NOTICE '[Client] Something failed in foreign table wipe (errcode: %, errm: %)', SQLSTATE, SQLERRM;
-    RETURN false;
-END;
-$$ LANGUAGE plpgsql;
-
---
--- Function to trigger augmented table deletion in the server after 
--- local augmentation has finished.
---
-
-CREATE OR REPLACE FUNCTION _mark_augmented_foreign_table_deletion(foreign_schema text, foreign_table text)
-RETURNS boolean AS $$
-    CONNECT _augmentation_server_conn_str();
-    SELECT * FROM _mark_user_augmented_table_deletion(foreign_schema::text, foreign_table::text);
+CREATE OR REPLACE FUNCTION _OBS_AugmentMeasureServer2Results(table_schema text, table_name text, tag_name text, normalize text, timespan text, geometry_level text)
+RETURNS SETOF record AS $$
+    CONNECT _server_conn_str();
 $$ LANGUAGE plproxy;
 
---
--- Internal function that alters the user table to add a new column
---
-
-CREATE OR REPLACE FUNCTION _add_augmented_column(table_name text, column_name text, data_type text)
-RETURNS text AS $$
-DECLARE
-  alter_str text;
-  new_column_name text;
-  epoch_timestamp text;
-BEGIN
-  SELECT extract(epoch from now() at time zone 'utc')::int INTO epoch_timestamp;
-  new_column_name := column_name || '_aug_' || epoch_timestamp;
-
-  -- Add column to table
-  alter_str := 'ALTER TABLE ' || table_name || ' ADD COLUMN ' || new_column_name || ' ' || data_type;
-  EXECUTE alter_str;
-
-  RETURN new_column_name;
-EXCEPTION
-  WHEN others THEN
-    RAISE NOTICE '[Client] The augmented column could not be included in your table. (errcode: %, errm: %)', SQLSTATE, SQLERRM;
-    RETURN null;
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION _OBS_DropForeignData(table_schema text, table_name text, server_name text)
+RETURNS boolean AS $$
+    CONNECT _server_conn_str();
+$$ LANGUAGE plproxy;
